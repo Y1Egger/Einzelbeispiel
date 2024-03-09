702b9d0f6420d0ad33c7a4b3490ed3e0ef8b99d5
Yvonne Egger <y1egger@edu.aau.at>
2024-03-09 19:43:06 +0100

    Client Server Connection

--- app/src/main/java/com/example/einzelbeispiel/MainActivity.java
+++ app/src/main/java/com/example/einzelbeispiel/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.einzelbeispiel;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,6 +14,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import java.io.BufferedReader;
+import java.io.DataOutput;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
@@ -26,6 +29,9 @@ public class MainActivity extends AppCompatActivity {
     private TextView txt;
     private Button btn;
 
+    String matNr;
+    String messageReceived = "";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,40 +42,45 @@ public class MainActivity extends AppCompatActivity {
         txt = findViewById(R.id.textView);
         btn = findViewById(R.id.button);
 
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                matNr = editTxt.getText().toString();
+                new NetworkCallTask().execute(matNr);
+            }
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        try {
-                            Socket socket = new Socket(serverName, serverPort);
-
-                            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                            String messageReceived = in.readLine();
-                            socket.close();
-
-                            runOnUiThread(new Runnable() {
-                                @Override
-                                public void run() {
-                                    txt.setText(messageReceived);
-                                }
-                            });
-
-                        } catch (IOException e) {
-                            throw new RuntimeException(e);
-                        }
-                    }
-                }).start();
-
-            }
-        });
-
     }//onCreate
+
+    public class NetworkCallTask extends AsyncTask<String, Void, String>{
+        @Override
+        protected String doInBackground(String... strings) {
+            try {
+                Socket socket = new Socket(serverName, serverPort);
+
+                DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+                out.writeBytes(strings[0] + "\n");
+                out.flush();
+
+                messageReceived = in.readLine();
+                socket.close();
+
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+            return messageReceived;
+        }
+
+        protected void onPostExecute(String messageReceived) {
+            txt.setText(messageReceived);
+        }
+    }
 }//main
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,4 +21,3 @@ dependencyResolutionManagement {
 
 rootProject.name = "Einzelbeispiel"
 include(":app")
- 
